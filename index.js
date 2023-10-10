fee64fe4c72c53d945425d377491a0610c183b94
Nyagol <nyagolpeter@gmail.com>
2023-10-10 14:37:06 +0300

    completed

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    const words = tutorial.split(" ");
+    const capitalizedWords = words.map((word) =>
+      word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    return capitalizedWords.join(" ");
+  });
+};
+
+const titleCasedTutorials = titleCased();
+console.log(titleCasedTutorials);
